2fce7b5192eadb46319f6cd16078eb6d07f68126
Liang Zhuang <bzhuangliang@gmail.com>
2026-06-01 22:49:50 -0700

    platform fee

--- NFTOrderBook/contracts/ProtocolFeeManager.sol
+++ NFTOrderBook/contracts/ProtocolFeeManager.sol
@@ -1,3 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ProtocolFeeManager is Ownable {
+    uint128 public protocolShare;
+    uint128 public constant MAX_PROTOCOL_SHARE = 1000;
+    
+    function setProtocolShare(uint128 newProtocolShare) external onlyOwner {
+        require(newProtocolShare <= MAX_PROTOCOL_SHARE, "ProtocolFeeManager: invalid protocol share");
+        protocolShare = newProtocolShare;
+    }
+}
